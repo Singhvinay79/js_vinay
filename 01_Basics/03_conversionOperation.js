56635285a55a3c99acff345419b0831666de9f0d
Singhvinay79 <103034803+Singhvinay79@users.noreply.github.com>
2024-09-11 01:48:27 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score ="777"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+/*
+"33" => 33
+"33abc"=>NaN
+true => 1; false => 0
+
+*/
+
+let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1=> true; 0 => false
+"" => false
+"vinay" => true 
+*/
+
+let someNumber = 11
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
