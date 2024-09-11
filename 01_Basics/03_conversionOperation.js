a5c3489dc8894e3a4bdd362870e7f4847e6e3326
Singhvinay79 <103034803+Singhvinay79@users.noreply.github.com>
2024-09-11 05:07:44 +0000

    comparision

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -28,5 +28,33 @@ let isLoggedIn = ""
 
 let someNumber = 11
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*********************************--Operations--******************* */
+
+let value = 3
+let negativeValue = -value
+
+// console.log(negativeValue);
+
+let str1 = "hello"
+let str2 = " vinay"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+console.log(true);
+console.log(+true);
+// console.log(true+);
+
+console.log(-true);
+
+
+
