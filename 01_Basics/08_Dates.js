01725cb362233be0d517acaaa8d5120afd734c96
Singhvinay79 <103034803+Singhvinay79@users.noreply.github.com>
2024-09-11 12:20:36 +0000

    date

---
+++ 01_Basics/08_Dates.js
@@ -0,0 +1,15 @@
+// ***************************- DATE -*********************
+
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log( typeof myDate);
+
+// ***************************************************
+
+const mycreatedDate =  new Date(2023,0,23);
+
+console.log(mycreatedDate);
+console.log(mycreatedDate.toString());
